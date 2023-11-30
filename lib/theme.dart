5b119b6ace37b004b640180cb6262a7c26fdd1b7
birajchhetri30 <birajchhetri3824@gmail.com>
2023-11-30 16:38:18 +0545

    Update theme.dart

--- lib/theme.dart
+++ lib/theme.dart
@@ -5,7 +5,7 @@ class CustomTheme {
     return ThemeData(
         colorScheme: const ColorScheme(
             brightness: Brightness.light,
-            primary: Color.fromARGB(193, 41, 34, 76),
+            primary: Color.fromARGB(193, 66, 54, 124),
             onPrimary: Colors.white,
             secondary: Color.fromARGB(210, 251, 229, 155),
             onSecondary: Colors.white,
