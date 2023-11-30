a1baf1e1758465343ca0f897b7a77994f5517284
birajchhetri30 <birajchhetri3824@gmail.com>
2023-11-30 15:38:34 +0545

    Created custom theme

--- lib/theme.dart
+++ lib/theme.dart
@@ -3,19 +3,19 @@ import 'package:flutter/material.dart';
 class CustomTheme {
   ThemeData getTheme() {
     return ThemeData(
-      colorScheme: const ColorScheme(
-        brightness: Brightness.light,
-        primary: Color.fromARGB(193, 52, 31, 100),
-        onPrimary: Colors.white,
-        secondary: Color.fromARGB(193, 227, 192, 96),
-        onSecondary: Colors.white,
-        background: Color.fromARGB(193, 75, 45, 146),
-        onBackground: Colors.grey,
-        surface: Colors.black,
-        onSurface: Colors.white,
-        error: Colors.red,
-        onError: Colors.white
-        )
-    );
+        colorScheme: const ColorScheme(
+            brightness: Brightness.light,
+            primary: Color.fromARGB(193, 41, 34, 76),
+            onPrimary: Colors.white,
+            secondary: Color.fromARGB(210, 251, 229, 155),
+            onSecondary: Colors.white,
+            background: Color.fromARGB(193, 90, 77, 156),
+            onBackground: Color.fromARGB(255, 190, 190, 190),
+            surface: Colors.black,
+            onSurface: Colors.white,
+            error: Colors.red,
+            onError: Colors.white),
+        useMaterial3: true,
+        fontFamily: 'Bahnschrift');
   }
 }
