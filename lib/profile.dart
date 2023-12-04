54bbe127195f662e344d9d64c229b17b13fd0e09
birajchhetri30 <birajchhetri3824@gmail.com>
2023-12-04 16:38:17 +0545

    Displays current user's blogs

--- lib/profile.dart
+++ lib/profile.dart
@@ -6,8 +6,6 @@ import 'package:blogapp/current_user.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-import 'dart:convert';
-
 class Profile extends StatefulWidget {
   const Profile({super.key});
 
@@ -25,42 +23,113 @@ class _ProfileState extends State<Profile> {
     var reuse = ReusableWidgets(context);
 
     var titleStyle = theme.textTheme.displayMedium!
-        .copyWith(color: theme.colorScheme.onBackground);
-    return Scaffold(
-      body: NestedScrollView(
-        headerSliverBuilder: (context, innerBoxIsScrolled) {
-          return <Widget>[
-            SliverAppBar(
-              expandedHeight: 250,
-              floating: true,
-              pinned: true,
-              toolbarHeight: 100,
-              stretch: true,
-              flexibleSpace: FlexibleSpaceBar(
-                title: Text("Hello ${CurrentUser.fname}", style: titleStyle),
-                centerTitle: false,
-                titlePadding: EdgeInsets.all(20),
-                stretchModes: [StretchMode.fadeTitle],
+        .copyWith(color: theme.colorScheme.onPrimary);
+    var textStyle = theme.textTheme.titleLarge!;
+    return NestedScrollView(
+      headerSliverBuilder: (context, innerBoxIsScrolled) {
+        return <Widget>[
+          SliverAppBar(
+            expandedHeight: 250,
+            floating: false,
+            pinned: true,
+            toolbarHeight: 100,
+            stretch: false,
+            backgroundColor: theme.colorScheme.background,
+            surfaceTintColor: theme.colorScheme.background,
+            flexibleSpace: Row(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                SizedBox(
+                  width: 300,
+                  child: FlexibleSpaceBar(
+                    title:
+                        Text("Hello ${CurrentUser.fname}", style: titleStyle),
+                    centerTitle: false,
+                    titlePadding: const EdgeInsets.all(20),
+                    //stretchModes: const [StretchMode.fadeTitle],
+                  ),
+                ),
+                const Spacer(),
+                Padding(
+                  padding: const EdgeInsets.all(15),
+                  child: reuse.createIconButton(
+                      icon: Icon(Icons.logout), onPressed: logoutAccount),
+                )
+              ],
+            ),
+          ),
+          SliverAppBar(
+            title: Padding(
+              padding: const EdgeInsets.only(left: 10.0),
+              child: Text("Your blogs", style: textStyle),
+            ),
+            centerTitle: false,
+            pinned: true,
+            backgroundColor: theme.colorScheme.background,
+            surfaceTintColor: theme.colorScheme.background,
+            toolbarHeight: 50,
+          )
+        ];
+      },
+      body: Container(
+        color: theme.colorScheme.background,
+        padding: const EdgeInsets.only(left: 10, right: 10),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Expanded(
+              child: ListView(
+                children: [
+                  for (var blog in CurrentUser.blogs)
+                    reuse.createCard(blog: blog)
+                ],
               ),
-              //onStretchTrigger: () {},
-              backgroundColor: theme.colorScheme.background,
-              //surfaceTintColor: theme.colorScheme.onBackground,
-            )
-          ];
-        },
-        body: Center(
-            child: reuse.createButton(
-                buttonText: "Sign out", onPressed: logoutAccount)),
+            ),
+          ],
+        ),
       ),
     );
   }
 
   void logoutAccount() async {
-    CurrentUser.fname = "";
-    CurrentUser.lname = "";
+    CurrentUser.reset();
+
     await FirebaseAuth.instance.signOut();
     Navigator.popUntil(context, (route) => route.isFirst);
     Navigator.pushReplacement(
         context, MaterialPageRoute(builder: (context) => const Login()));
+  }
+}
+
+class _SliverAppBarDelegate extends SliverPersistentHeaderDelegate {
+  _SliverAppBarDelegate(this._text);
+
+  final Text _text;
+
+  @override
+  double get minExtent => 31;
+
+  @override
+  double get maxExtent => 31;
+
+  @override
+  Widget build(
+      BuildContext context, double shrinkOffset, bool overlapsContent) {
+    var theme = Theme.of(context);
+    return SizedBox(
+      height: 50,
+      child: Container(
+        color: theme.colorScheme.background,
+        child: Padding(
+          padding: const EdgeInsets.only(left: 25),
+          child: _text,
+        ),
+      ),
+    );
+  }
+
+  @override
+  bool shouldRebuild(_SliverAppBarDelegate oldDelegate) {
+    return false;
   }
 }
