56f5aeb6dedecdcdf745878cbe237814ac3471b7
birajchhetri30 <birajchhetri3824@gmail.com>
2023-12-04 16:37:14 +0545

    Fixed color bugs

--- lib/home.dart
+++ lib/home.dart
@@ -21,61 +21,65 @@ class _HomeState extends State<Home> {
     var reuse = ReusableWidgets(context);
     var theme = Theme.of(context);
 
-    return Scaffold(
-        body: currPage,
-        floatingActionButton: SizedBox(
-          width: 65,
-          height: 65,
-          child: FloatingActionButton(
-            onPressed: () {
-              Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => CreateBlog()));
-            },
-            shape: const CircleBorder(),
-            elevation: 10,
-            backgroundColor: theme.colorScheme.secondary,
-            child: const Icon(Icons.add, size: 35),
+    return Container(
+      color: theme.colorScheme.background,
+      child: Scaffold(
+          body: currPage,
+          floatingActionButton: SizedBox(
+            width: 65,
+            height: 65,
+            child: FloatingActionButton(
+              onPressed: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => CreateBlog()));
+              },
+              shape: const CircleBorder(),
+              elevation: 10,
+              backgroundColor: theme.colorScheme.secondary,
+              child: const Icon(Icons.add, size: 35),
+            ),
           ),
-        ),
-        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-        bottomNavigationBar: BottomAppBar(
-          color: theme.colorScheme.primary,
-          shape: const CircularNotchedRectangle(),
-          notchMargin: 8,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              reuse.createIconButton(
-                  icon: Icon(Icons.home_rounded),
-                  onPressed: () {
-                    setState(() {
-                      currPage = Feed();
-                    });
-                  }),
-              reuse.createIconButton(
-                  icon: Icon(Icons.notifications),
-                  onPressed: () {
-                    setState(() {
-                      currPage = Notifications();
-                    });
-                  }),
-              const SizedBox(width: 50),
-              reuse.createIconButton(
-                  icon: Icon(Icons.search),
-                  onPressed: () {
-                    setState(() {
-                      currPage = Search();
-                    });
-                  }),
-              reuse.createIconButton(
-                  icon: Icon(Icons.person),
-                  onPressed: () {
-                    setState(() {
-                      currPage = Profile();
-                    });
-                  }),
-            ],
-          ),
-        ));
+          floatingActionButtonLocation:
+              FloatingActionButtonLocation.centerDocked,
+          bottomNavigationBar: BottomAppBar(
+            color: theme.colorScheme.primary,
+            shape: const CircularNotchedRectangle(),
+            notchMargin: 8,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                reuse.createIconButton(
+                    icon: const Icon(Icons.home_rounded),
+                    onPressed: () {
+                      setState(() {
+                        currPage = Feed();
+                      });
+                    }),
+                reuse.createIconButton(
+                    icon: Icon(Icons.notifications),
+                    onPressed: () {
+                      setState(() {
+                        currPage = Notifications();
+                      });
+                    }),
+                const SizedBox(width: 50),
+                reuse.createIconButton(
+                    icon: Icon(Icons.search),
+                    onPressed: () {
+                      setState(() {
+                        currPage = Search();
+                      });
+                    }),
+                reuse.createIconButton(
+                    icon: Icon(Icons.person),
+                    onPressed: () {
+                      setState(() {
+                        currPage = Profile();
+                      });
+                    }),
+              ],
+            ),
+          )),
+    );
   }
 }
