443519890642018d991f05c0ec92b8ad5a913cf2
birajchhetri30 <birajchhetri3824@gmail.com>
2023-12-06 16:09:46 +0545

    Displays blogs from other users

--- lib/feed.dart
+++ lib/feed.dart
@@ -1,13 +1,36 @@
+import 'package:blogapp/current_user.dart';
+import 'package:blogapp/main.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-class Feed extends StatelessWidget {
+class Feed extends StatefulWidget {
+  const Feed({super.key});
+
+  @override
+  State<Feed> createState() => _FeedState();
+}
+
+class _FeedState extends State<Feed> {
   @override
   Widget build(BuildContext context) {
     var theme = Theme.of(context);
+    var appState = context.watch<CurrentUser>();
+    var reuse = ReusableWidgets(context);
+
+    var appTitleStyle =
+        theme.textTheme.displaySmall!.copyWith(fontFamily: "Typographica");
 
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Home"),
+        title: Text("BlogVerse", style: appTitleStyle),
+        backgroundColor: theme.colorScheme.primary,
+        toolbarHeight: 70,
+      ),
+      body: ListView(
+        padding: const EdgeInsets.only(top: 8, bottom: 30),
+        children: [
+          for (var blog in appState.getFeedBlogs()) reuse.createCard(blog: blog)
+        ],
       ),
     );
   }
