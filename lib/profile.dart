8b6c20b1590cfe4867b04509643fb1ac5d2a605c
birajchhetri30 <birajchhetri3824@gmail.com>
2023-12-13 14:12:41 +0545

    Update profile.dart

--- lib/profile.dart
+++ lib/profile.dart
@@ -1,7 +1,6 @@
 import 'package:blogapp/drafts.dart';
 import 'package:blogapp/follow.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 import 'package:flutter_phoenix/flutter_phoenix.dart';
 import 'package:provider/provider.dart';
 
